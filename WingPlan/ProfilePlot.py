f4af52d66ad75fba21d146fd97cc2ada14bf84f3
bobm123 <bobm123@gmail.com>
2016-08-28 09:05:26 -0400

    Simplify generation of the path command string

--- WingPlan/ProfilePlot.py
+++ WingPlan/ProfilePlot.py
@@ -17,24 +17,13 @@ def ProfilePlot(arguments, chord=100):
 
     print(infile.readline())
     polar = [[chord*float(c) for c in line.split()] for line in infile]
-    #polar.append(polar[0])
 
     dwg = svgwrite.Drawing(arguments['<outfile>'], profile='tiny')
 
     #offset = [10, 100]
-    #for i in range(len(polar)-1):
-    #    p0 = [sum(x) for x in zip(polar[i], offset)]
-    #    p1 = [sum(x) for x in zip(polar[i+1], offset)]
-    #    dwg.add(dwg.line(p0, p1, stroke=svgwrite.rgb(10, 0, 0)))
-
-    # This nifty patter generates the basic cmds, but leaves it open
-    # and not sure I like index with a bollean
-    #d = ' '.join(['%s%d %d' % (['M', 'L'][i>0], x, y) for i, (x, y) in enumerate(coord_list)])
 
     # Initial move, add the lines, the close
-    path_cmd = "M{},{}".format(polar[0][0], polar[0][1])
-    path_cmd = path_cmd+''.join(["L{},{}".format(p[0], p[1]) for p in polar[1:]])
-    path_cmd = path_cmd+"Z"
+    path_cmd = 'M'+'L'.join(["{} {}".format(*p) for p in polar])+'Z'
 
     # Add the path to the drawing and save
     polar_path = svgwrite.path.Path(d=path_cmd, fill='none', stroke="#FF0000");
