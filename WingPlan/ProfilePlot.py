b2ba264073cb0cd83bfb7048e4535cc636df6411
bobm123 <bobm123@gmail.com>
2016-08-29 06:40:01 -0400

    break out file read into its own function

--- WingPlan/ProfilePlot.py
+++ WingPlan/ProfilePlot.py
@@ -10,35 +10,47 @@ import svgwrite
 from docopt import docopt
 
 
-def ProfilePlot(arguments, chord=100, offset=(10,100)):
+def read_polars(infile, chord, invert_y=False):
+
+    #Skips (and prints) airoil name
+    print(infile.readline())
+    polars = [[chord*float(c) for c in line.split()] for line in infile]
+
+    iy = -1 if invert_y == True else 1
+    polars = [(p[0], iy*p[1]) for p in polars if len(p) == 2]
+
+    return polars
+
+
+
+def profile_plot(arguments, chord=100, offset=(10,100)):
     infile = open(arguments['<infile>'], 'r')
     if not arguments['<outfile>']:
         arguments['<outfile>'] = arguments['<infile>']+'.svg'
 
-    print(infile.readline())
-    polar = [[chord*float(c) for c in line.split()] for line in infile]
-    polar = [(p[0], -p[1]) for p in polar if len(p) == 2]
+    polars = read_polars(infile, chord, invert_y=True)
 
     # Calculate size of a bounding box
-    polar_min = [a for a in map(min, zip(*polar))]
-    polar_max = [a for a in map(max, zip(*polar))]
+    polar_min = [a for a in map(min, zip(*polars))]
+    polar_max = [a for a in map(max, zip(*polars))]
     polar_size = [polar_max[0]-polar_min[0], polar_max[1]-polar_min[1]]
-    print(polar_min)
-    print(polar_size)
+    #print(polar_min)
+    #print(polar_size)
 
-    dwg = svgwrite.Drawing(arguments['<outfile>'], size=('170mm', '130mm'), viewBox=('0 0 170 130'))
+    svg_extras = {'fill': 'none', 'stroke_width': .25}
+    dwg = svgwrite.Drawing(arguments['<outfile>'], profile='tiny', size=('170mm', '130mm'), viewBox=('0 0 170 130'))
     grp = svgwrite.container.Group(transform='translate({},{})'.format(*offset))
     dwg.add(grp)
 
     # Initial move, add the lines, the close
-    path_cmd = 'M'+'L'.join(["{} {}".format(*p) for p in polar])+'Z'
+    path_cmd = 'M'+'L'.join(["{} {}".format(*p) for p in polars])+'Z'
 
     # Add the path to the drawing and save
-    polar_path = svgwrite.path.Path(d=path_cmd, fill='none', stroke="#FF0000", stroke_width=.25);
+    polar_path = svgwrite.path.Path(d=path_cmd, stroke="#FF0000", **svg_extras);
     grp.add(polar_path);
 
     # Add a bounding box
-    bounds_rect = svgwrite.shapes.Rect(insert=polar_min, size=polar_size, fill='none', stroke="#0000FF", stroke_width=.25)
+    bounds_rect = svgwrite.shapes.Rect(insert=polar_min, size=polar_size, stroke="#0000FF", **svg_extras)
     grp.add(bounds_rect)
 
     dwg.save()
@@ -46,4 +58,4 @@ def ProfilePlot(arguments, chord=100, offset=(10,100)):
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, help=True, version='Profile Plot 0.0')
-    ProfilePlot(arguments)
+    profile_plot(arguments)
