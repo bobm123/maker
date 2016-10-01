6a9c6e96a7eb98dd2f07e175b3f39bebe44510d0
Robert Marchese <bobm123@gmail.com>
2016-10-01 07:26:26 -0400

    Get svg filename from commands line.

--- WingPlan/SvgViewer.py
+++ WingPlan/SvgViewer.py
@@ -1,26 +1,43 @@
-# Import PySide classes
+"""
+Profile Plot
+Usage:
+  SvgViewer.py <svgfile>
+"""
 import sys
 from PySide import QtCore, QtGui, QtOpenGL, QtSvg
+from docopt import docopt
 
-# Create a Qt application
-app = QtGui.QApplication(sys.argv)
 
-# Create a Window
-mywindow = QtGui.QWidget()
-mywindow.resize(425, 200)
-mywindow.setWindowTitle('SVG Viewer')
+def SvgViewer(argv):
+    svgFilename = arguments['<svgfile>']
 
-#TODO: Get filename fomr UI or command line
-fileName = 'clarky.dat.svg'
+    # Create a Qt application
+    app = QtGui.QApplication('')
 
-# Create a label and display it all together
-mylabel = QtGui.QLabel(mywindow)
-mylabel.setText(fileName)
-mylabel.setGeometry(QtCore.QRect(10, 10, 80, 15))
+    # Create a Window
+    mywindow = QtGui.QWidget()
+    mywindow.resize(425, 200)
+    mywindow.setWindowTitle('SVG Viewer')
 
-mysvg_widget = QtSvg.QSvgWidget(fileName, mywindow)
+    # Create a label and display it all together
+    mylabel = QtGui.QLabel(mywindow)
+    mylabel.setGeometry(QtCore.QRect(10, 10, 80, 15))
+    mylabel.setText(svgFilename)
 
-mywindow.show()
+    mysvg_widget = QtSvg.QSvgWidget(svgFilename, mywindow)
+    print(mysvg_widget.size())
+    print(mysvg_widget.widthMM(), mysvg_widget.heightMM())
 
-# Enter Qt application main loop
-sys.exit(app.exec_())
+    spos = QtCore.QPoint(10,10)
+    ssize= QtCore.QSize(mysvg_widget.size())
+
+    mywindow.show()
+
+    # Enter Qt application main loop
+    sys.exit(app.exec_())
+
+    
+if __name__ == '__main__':
+    arguments = docopt(__doc__, help=True, version='Svg Viewer 0.0')
+    SvgViewer(arguments)
+    
