70a837aae3cf58263abe57c59f8edb5d31967c90
Robert Marchese <bobm123@gmail.com>
2016-10-01 09:38:27 -0400

    Initial investigaitons on setting window size based on SVG size

--- WingPlan/SvgViewer.py
+++ WingPlan/SvgViewer.py
@@ -25,19 +25,21 @@ def SvgViewer(argv):
     mylabel.setText(svgFilename)
 
     mysvg_widget = QtSvg.QSvgWidget(svgFilename, mywindow)
+    #mysvg_widget.setGeometry(QtCore.QRect(10, 10, 80, 15))
     print(mysvg_widget.size())
     print(mysvg_widget.widthMM(), mysvg_widget.heightMM())
+    print(mysvg_widget.rect())
 
-    spos = QtCore.QPoint(10,10)
-    ssize= QtCore.QSize(mysvg_widget.size())
+    #TODO: resize window based on SVG size
+    #mywindow.resize( ... )
 
     mywindow.show()
 
     # Enter Qt application main loop
     sys.exit(app.exec_())
-
     
+
 if __name__ == '__main__':
     arguments = docopt(__doc__, help=True, version='Svg Viewer 0.0')
     SvgViewer(arguments)
-    
+
