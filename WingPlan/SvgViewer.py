2d35b2b61393bb073a0523ee96b112581cb29d8d
bobm123 <bobm123@gmail.com>
2016-11-12 16:43:56 -0500

    Adds recent files to menubar

--- WingPlan/SvgViewer.py
+++ WingPlan/SvgViewer.py
@@ -79,27 +79,140 @@ class SvgViewer(QtGui.QGraphicsView):
                 self.scale(*self._scale)
 
 
-class Window(QtGui.QWidget):
+#class Window(QtGui.QWidget):
+class MainWindow(QtGui.QMainWindow):
+    MaxRecentFiles = 9
+    windowList = []
+
     def __init__(self):
-        super(Window, self).__init__()
+        super(MainWindow, self).__init__()
+
+        self.recentFileActs = []
+
+        self.centralwidget = QtGui.QWidget()
         self.viewer = SvgViewer(self)
         self.edit = QtGui.QLineEdit(self)
         self.edit.setReadOnly(True)
         self.button = QtGui.QToolButton(self)
         self.button.setText('...')
         self.button.clicked.connect(self.handleOpen)
-        layout = QtGui.QGridLayout(self)
+        layout = QtGui.QGridLayout(self.centralwidget)
         layout.addWidget(self.viewer, 0, 0, 1, 2)
         layout.addWidget(self.edit, 1, 0, 1, 1)
         layout.addWidget(self.button, 1, 1, 1, 1)
+        self.setCentralWidget(self.centralwidget)
 
-    def handleOpen(self):
-        path = QtGui.QFileDialog.getOpenFileName(
-            self, 'Choose Image', self.edit.text())
-        print(path)
-        if path:
-            self.edit.setText(path[0])
-            self.viewer.setFilePath(path[0])
+        self.createActions()
+        self.createMenus()
+
+
+    def handleOpen(self, fileName):
+        self.edit.setText(fileName)
+        self.viewer.setFilePath(fileName)
+        self.setCurrentFile(fileName)
+
+
+    def open(self):
+        fileName, filtr = QtGui.QFileDialog.getOpenFileName(self)
+        if fileName:
+            self.handleOpen(fileName)
+
+    def openRecentFile(self):
+        action = self.sender()
+        if action:
+            self.handleOpen(action.data())
+
+    def about(self):
+        QtGui.QMessageBox.about(self, "About SVG File Viewer",
+                "A lightweight SVG file viewer that allows you "
+                "to inspect drawings and take a closer look at "
+                "sections of them with the pan and zoom tools")
+
+    def createActions(self):
+        self.openAct = QtGui.QAction("&Open...", self,
+                shortcut=QtGui.QKeySequence.Open,
+                statusTip="Open an svg file", triggered=self.open)
+
+        self.aboutAct = QtGui.QAction("&About", self,
+                statusTip="Show the application's About box",
+                triggered=self.about)
+
+        for i in range(MainWindow.MaxRecentFiles):
+            self.recentFileActs.append(
+                    QtGui.QAction(self, visible=False,
+                            triggered=self.openRecentFile))
+
+        self.exitAct = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q",
+                statusTip="Exit the application",
+                triggered=QtGui.qApp.closeAllWindows)
+
+
+    def createMenus(self):
+        self.fileMenu = self.menuBar().addMenu("&File")
+        #self.fileMenu.addAction(self.newAct)
+        self.fileMenu.addAction(self.openAct)
+        #self.fileMenu.addAction(self.saveAct)
+        #self.fileMenu.addAction(self.saveAsAct)
+
+        self.separatorAct = self.fileMenu.addSeparator()
+        for i in range(MainWindow.MaxRecentFiles):
+            self.fileMenu.addAction(self.recentFileActs[i])
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+        self.updateRecentFileActions()
+
+        self.menuBar().addSeparator()
+        self.helpMenu = self.menuBar().addMenu("&Help")
+        self.helpMenu.addAction(self.aboutAct)
+
+    def setCurrentFile(self, fileName):
+        self.curFile = fileName
+        if self.curFile:
+            self.setWindowTitle("SvgViewer - {}".format(self.strippedName(self.curFile)))
+        else:
+            self.setWindowTitle("SvgViewer")
+
+        settings = QtCore.QSettings('Maker', 'SvgViewer')
+        files = settings.value('recentFileList', [])
+        if isinstance(files, str):
+            files = [files] #special case when a single file
+
+        try:
+            files.remove(fileName)
+        except ValueError:
+            pass
+
+        files.insert(0, fileName)
+        del files[MainWindow.MaxRecentFiles:]
+
+        settings.setValue('recentFileList', files)
+
+        for widget in QtGui.QApplication.topLevelWidgets():
+            if isinstance(widget, MainWindow):
+                widget.updateRecentFileActions()
+
+
+    def updateRecentFileActions(self):
+        settings = QtCore.QSettings('Maker', 'SvgViewer')
+        files = settings.value('recentFileList')
+        files_no = 0
+        if files:
+            files_no = len(files)
+        numRecentFiles = min(files_no, MainWindow.MaxRecentFiles)
+
+        for i in range(numRecentFiles):
+            text = "&%d %s" % (i + 1, self.strippedName(files[i]))
+            self.recentFileActs[i].setText(text)
+            self.recentFileActs[i].setData(files[i])
+            self.recentFileActs[i].setVisible(True)
+
+        for j in range(numRecentFiles, MainWindow.MaxRecentFiles):
+            self.recentFileActs[j].setVisible(False)
+
+        self.separatorAct.setVisible((numRecentFiles > 0))
+
+    def strippedName(self, fullFileName):
+        return QtCore.QFileInfo(fullFileName).fileName()
 
 
 if __name__ == '__main__':
@@ -109,8 +222,8 @@ if __name__ == '__main__':
 
     import sys
     app = QtGui.QApplication(sys.argv)
-    window = Window()
-    window.setGeometry(500, 300, 822, 651)
-    window.show()
+    main_window = MainWindow()
+    main_window.setGeometry(500, 300, 822, 651)
+    main_window.show()
     sys.exit(app.exec_())
 
