48f4a2a5703cc9e6038c3b45f6c4709c6da98ec9
bobm123 <bobm123@gmail.com>
2017-02-20 05:17:49 -0500

    path to OpenSCAD for linux

--- winder/makeStl.py
+++ winder/makeStl.py
@@ -22,31 +22,32 @@ def gen_stl_files(project, parts_list, scad_path):
         outfile.write("use <%s>\n%s_stl();\n" % (project, part_name))
         outfile.close()
         cmd = '%s -o %s.stl __%s.scad' % (scad_path, part_name, part_name)
-        print(cmd)
         cmdList.append(cmd)
 
     for cmd in cmdList:
+        print(cmd)
         call(cmd, shell=True)
 
         
-def main(argv, argc):
+def main(argv):
     '''
     Generates .stl files from 'parts' (modules with names 
     ending in _stl) found in a .scad file.
     '''
-    if argc < 1:
+    if len(argv) < 2:
         print("generates .stl files from 'parts' (modules with names")
         print("ending in _stl) found in a .scad file")
         print("usage:\n\tpython makeStl.py myproject.scad [path to openSCAD]")
         exit(0)
         
     argv.append('')    
-    scad_path = os.path.join(argv[2], 'openscad')
+    #scad_path = os.path.join(argv[2], 'openscad')
+    scad_path = '/usr/bin/openscad'
     parts_list = find_parts(argv[1])
     print(parts_list)
     gen_stl_files(argv[1], parts_list, scad_path)
 
     
 if __name__ == "__main__":
-    main(sys.argv, len(sys.argv)-1)
+    main(sys.argv)
     
